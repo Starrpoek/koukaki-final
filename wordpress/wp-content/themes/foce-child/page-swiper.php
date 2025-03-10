dab452b6d5a3af451fb9a55f44e4b6b45e850e82
Starrpoek <163408126+Starrpoek@users.noreply.github.com>
2025-03-10 19:33:32 +0100

    Update page-swiper.php

--- wordpress/wp-content/themes/foce-child/page-swiper.php
+++ wordpress/wp-content/themes/foce-child/page-swiper.php
@@ -1,23 +1,23 @@
 <div class="swiper mySwiper">
     <div class="swiper-wrapper">
         <div class="swiper-slide">
-          <img src="http://localhost/koukaki-final/wordpress/wp-content/uploads/2022/06/Kawaneko.png" />
+          <img src="http://darkblue-wolf-460890.hostingersite.com/koukaki-final/wordpress/wp-content/uploads/2022/06/Kawaneko.png" />
           <p>Kawaneko</p>
         </div>
         <div class="swiper-slide">
-          <img src="http://localhost/koukaki-final/wordpress/wp-content/uploads/2022/06/Orenjiiro-1.png" />
+          <img src="http://darkblue-wolf-460890.hostingersite.com/koukaki-final/wordpress/wp-content/uploads/2022/06/Orenjiiro-1.png" />
           <p>Orenjiiro</p>
         </div>
         <div class="swiper-slide">
-          <img src="http://localhost/koukaki-final/wordpress/wp-content/uploads/2022/06/Pinku-1.png" />
+          <img src="http://darkblue-wolf-460890.hostingersite.com/koukaki-final/wordpress/wp-content/uploads/2022/06/Pinku-1.png" />
           <p>Pinku</p>
         </div>
         <div class="swiper-slide">
-          <img src="http://localhost/koukaki-final/wordpress/wp-content/uploads/2022/06/Tenshi-1.png" />
+          <img src="http://darkblue-wolf-460890.hostingersite.com/koukaki-final/wordpress/wp-content/uploads/2022/06/Tenshi-1.png" />
           <p>Tenshi</p>
         </div>
         <div class="swiper-slide">
-          <img src="http://localhost/koukaki-final/wordpress/wp-content/uploads/2022/06/Jaakuna-1.png" />
+          <img src="http://darkblue-wolf-460890.hostingersite.com/koukaki-final/wordpress/wp-content/uploads/2022/06/Jaakuna-1.png" />
           <p>Kaakuna</p>
         </div>
     </div>
